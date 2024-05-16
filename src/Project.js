97f32b08699e20ecf9d8f5648d72a3cb29c55069
rei <thogasec@gmail.com>
2024-05-16 14:26:05 +0800

    project page

---
+++ src/Project.js
@@ -0,0 +1,15 @@
+export function Project() {
+    return (
+        <div class="container">
+            <div class="title-block">
+                <img src="https://pbs.twimg.com/media/GM_BJAtWUAAa2ee?format=jpg&name=900x900" alt="project pic" />
+                <span class="p-title centered">科技應用之數位教材</span>
+            </div>
+
+            <div class="content">
+                <h1>About Us</h1>
+                <p>項目概述： 智能語言學習平台是一個旨在幫助使用者輕鬆學習語言的線上平台。透過結合人工智能和語言學習技術，我們提供了一種創新的學習方式，使使用者能夠以更加愉快和高效的方式掌握目標語言。 </p>
+            </div>
+        </div>
+    );
+};
