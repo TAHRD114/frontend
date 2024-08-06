d3ef71a30d02ca6b1442af4db784a13c79cb4793
ssutsen <babe920604@gmail.com>
2024-08-06 16:10:09 +0800

    feat: update with vite

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Project from "./pages/Project";
 import BlogPage from "./pages/BlogPage";
 import About from "./pages/AboutPage";
 import Home from "./pages/Home";
+import Footer from "./pages/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/blog" element={<BlogPage />} />
           <Route path="/aboutUs" element={<About />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
